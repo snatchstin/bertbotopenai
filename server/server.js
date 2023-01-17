5dcebb8a633c373a4c2977270f2a28a4a4e28dbc
snatchstin <astroid882@gmail.com>
2023-01-16 17:18:07 -0800

    added bert back

--- server/server.js
+++ server/server.js
@@ -27,12 +27,14 @@ app.post("/", async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      prompt:
+        "The following is a conversation with an AI assistant named Bert. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nBert: I am an AI created by Ashtin Evans using GPT-3. How can I help you today?\nHuman: ",
+      temperature: 0.9,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+      stop: [" Human:", "Bert"],
       user: "bertbotai99",
     });
 
